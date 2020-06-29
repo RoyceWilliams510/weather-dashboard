635318e388d44484d31119f0b06bb8c42d25c6af
Royce Williams <roycejames1130@gmail.com>
2020-06-29 12:15:17 -0700

    added the function that will save submitted cities if they are valid and return an alert if they are false

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=6f02d570f4546e29df74e2c093e143c5";
-    // Scream Ushindi (victory in Swahili)! at the top of your lungs
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response){
-      console.log(response);
-    })
+    if(JSON.parse(localStorage.getItem("savedCities")) !==null){
+        savedCities = JSON.parse(localStorage.getItem("savedCities"));
+    }
 
 })
+
+// global variables 
+var myKey = "6f02d570f4546e29df74e2c093e143c5"
+var isCity = true;
+var savedCities = [];
+
 // On click this function submits the text value in the text box
 function submitCity(){
     // this variable stores the data
@@ -18,9 +19,9 @@ function submitCity(){
     $("#cityInput").val("");
     console.log(cityName);
     // Need to check if the city has a result when searched
-
+    checksCity(cityName);
     // Need to save it in local storage
-
+    createList();
     // Need to append the city name to the table
 
     // Need to change the weather displayed
@@ -37,8 +38,30 @@ function createList(){
 // checks if the submitted city has a corresponding result when searched
 function checksCity(location){
     // this takes the argument of location which is the submitted string from the user input
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&appid=6f02d570f4546e29df74e2c093e143c5";
+    $.ajax({
+        statusCode:{
+            404: function() {
+                alert('There are no citys that goes by this name');
+                isCity = false;
+                console.log(isCity);
+                return;
+            },
+            500: function() {
+                alert('500 status code! server error');
+            }
+        },
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        console.log(response);
+        if(isCity === true){
+            savedCities.push(location)
+            localStorage.setItem("savedCities",JSON.stringify(savedCities));
+        }
+        isCity = true;
+      })
 
-    
 
 }
 
